0d03115f271c4056c9ea2c7a9296b51b0d8e7be5
reeteshdcd <reeteshkumar2407@gmail.com>
2025-11-02 14:13:12 +0530

    remove function

--- basic.py
+++ basic.py
@@ -1,4 +1,4 @@
 w = 'phool'
 print(f"{w} ne {w} kar {w} se kaha, {w} laye kis liye, Tum bhi to {w} ho.")
-def greet(name):
-    return f"Hello, {name}!"
+# def greet(name):
+#     return f"Hello, {name}!"
